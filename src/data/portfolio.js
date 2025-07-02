119f8b43350d0618806bd5902a07e4b119e2778d
nas225 <nas225@lehigh.edu>
2025-07-02 18:19:01 +0200

    changed order in portfolio section

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,14 +1,6 @@
 // src/data/portfolio.js
 export const portfolio = [
   {
-    tag: "Portfolio sajt",
-    title: "Privatni časovi Engleskog",
-    description:
-      "Višejezični sajt za privatnog nastavnika engleskog jezika sa sekcijama o iskustvu i ponudama, kontakt ikonama, responsivnom navigacijom i dropdown jezičkim mjenjačem.",
-    image: "/portfolio-1.png",
-    url: "https://charlesturner.netlify.app",
-  },
-  {
     tag: "Web katalog",
     title: "Šećerna pasta A47",
     description:
@@ -16,12 +8,12 @@ export const portfolio = [
     image: "/portfolio-2.png",
     url: "https://www.secernapasta.me",
   },
-  // {
-  //   tag: "Portfolio",
-  //   title: "Craft & Co Accessories",
-  //   description:
-  //     "Jednostranični portfolio s galerijom proizvoda, lightbox efektom i Typeform kontakt formom.",
-  //   image: "/portfolio-3.jpg",
-  //   url: "#",
-  // },
+  {
+    tag: "Portfolio sajt",
+    title: "Privatni časovi Engleskog",
+    description:
+      "Višejezični sajt za privatnog nastavnika engleskog jezika sa sekcijama o iskustvu i ponudama, kontakt ikonama, responsivnom navigacijom i dropdown jezičkim mjenjačem.",
+    image: "/portfolio-1.png",
+    url: "https://charlesturner.netlify.app",
+  },
 ];
